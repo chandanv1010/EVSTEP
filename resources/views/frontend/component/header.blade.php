bbdeea5157fcc3ab64acf1b01e42df12a5ed5dc4
chandanv1010 <tuannc.dev@gmail.com>
2025-12-04 16:47:28 +0700

    Fix

--- resources/views/frontend/component/header.blade.php
+++ resources/views/frontend/component/header.blade.php
@@ -184,6 +184,20 @@
                     </form>
                 </div>
                 <div class="tool">
+                    <div class="mobile-contacts">
+                        @if(isset($system['contact_hotline']) && !empty($system['contact_hotline']))
+                        <a href="tel:{{ $system['contact_hotline'] }}" class="mobile-contact-item mobile-hotline" title="Hotline">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+                            <span>{{ $system['contact_hotline'] }}</span>
+                        </a>
+                        @endif
+                        @if(isset($system['contact_email']) && !empty($system['contact_email']))
+                        <a href="mailto:{{ $system['contact_email'] }}" class="mobile-contact-item mobile-email" title="Email">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                            <span>{{ $system['contact_email'] }}</span>
+                        </a>
+                        @endif
+                    </div>
                     <div class="cart-link">
                         <a href="{{ write_url('gio-hang') }}" title="" class="nav-link">
                             <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" viewBox="0 0 576 512" class="w-5 h-5 cursor-pointer fill-bottom-nav-mb  box-content"><path d="M0 24C0 10.7 10.7 0 24 0h45.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5l-51.6-271c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24m128 440a48 48 0 1 1 96 0 48 48 0 1 1-96 0m336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96"></path></svg>
